738d761e524fec45cde15504d2ac8aa4551770c8
jiosaa <batisileshi11@gmail.com>
2022-10-01 09:30:33 -0400

    hosting

--- Visit_Oromia/settings.py
+++ Visit_Oromia/settings.py
@@ -129,6 +129,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 MEDIA_ROOT=os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
