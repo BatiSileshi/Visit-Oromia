962182cf5f211d81758306fc71626463d4cd3c10
jiosaa <batisileshi11@gmail.com>
2022-10-01 08:58:36 -0400

    hosting

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Visit_Oromia.wsgi --log-file -
--- Visit_Oromia/settings.py
+++ Visit_Oromia/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mid4q-br&x*$)=dh%vuai%9j#0$648#s8p9opf!qgd1_4nug%#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1', 'https://visit-oromia.herokuapp.com']
 
 # Application definition
 
@@ -46,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
