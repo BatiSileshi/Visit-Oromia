d2d811e4f84a59bb71f255b5d1de2715c8dbc037
jiosaa <batisileshi11@gmail.com>
2022-10-01 09:40:44 -0400

    hosting

--- Visit_Oromia/settings.py
+++ Visit_Oromia/settings.py
@@ -124,12 +124,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL='/images/'
-# STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 MEDIA_ROOT=os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
