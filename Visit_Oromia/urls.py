ab2e1acbc4d60058575b4f7f38d81e78c4e0340b
jiosaa <batisileshi93@gmail.com>
2022-10-14 22:32:23 -0400

    debug=false

--- Visit_Oromia/urls.py
+++ Visit_Oromia/urls.py
@@ -22,7 +22,7 @@ from django.views.static import serve
 from django.urls import re_path
 
 urlpatterns = [
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    re_path(r'^images/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}), 
     
     
