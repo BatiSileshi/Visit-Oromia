76278201c4842a85c92942e189e918f4c1961220
jiosaa <batisileshi93@gmail.com>
2022-10-14 21:35:37 -0400

    debug=false

--- Visit_Oromia/settings.py
+++ Visit_Oromia/settings.py
@@ -128,6 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
 MEDIA_ROOT=os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
