4fef6d53af3e774c2cfe40f1cd506a049e351c67
jiosaa <batisileshi93@gmail.com>
2022-10-14 22:40:56 -0400

    debug=false

--- Visit_Oromia/settings.py
+++ Visit_Oromia/settings.py
@@ -136,7 +136,7 @@ else:
   STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-MEDIA_ROOT=os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT=os.path.join(BASE_DIR, 'images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
