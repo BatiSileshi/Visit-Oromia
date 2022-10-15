8add64a101f44e11f977fd171d719149c8b7433c
jiosaa <batisileshi93@gmail.com>
2022-10-14 22:36:59 -0400

    debug=false

--- Visit_Oromia/settings.py
+++ Visit_Oromia/settings.py
@@ -133,7 +133,7 @@ if DEBUG:
 
 else:
 
-  STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 MEDIA_ROOT=os.path.join(BASE_DIR, 'static/images')
